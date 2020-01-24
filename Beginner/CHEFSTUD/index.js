a3dc602c1437dbe3e5dfa51ea8e06b36977d920f
happytejasrathod <happytejasrathod@gmail.com>
2020-01-24 19:40:41 +0530

    Beginner - Chef and his Students

---
+++ Beginner/CHEFSTUD/index.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var arr = '';
+process.stdin.on('data', function (chunk) {
+    arr += chunk;
+});
+
+process.stdin.on('end', function () {
+    arr = arr.split('\n');
+    var t = Number(arr.shift());
+    loop: for (var i = 0; i < t; i++) {
+      let str = arr.shift();
+      console.log(str.split('<>').length-1);
+    }
+});
