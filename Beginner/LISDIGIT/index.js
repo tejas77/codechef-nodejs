b0f9100def1c64d99a614430f2472e996bace52e
happytejasrathod <happytejasrathod@gmail.com>
2020-06-14 21:34:06 +0530

    Beginner - Digit Longest Increasing Subsequences 2

---
+++ Beginner/LISDIGIT/index.js
@@ -0,0 +1,15 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var arr = '';
+process.stdin.on('data', function (chunk) {
+    arr += chunk;
+});
+
+process.stdin.on('end', function () {
+    arr = arr.split('\n');
+    let t = Number(arr.shift());
+    loop: for (let s = 0; s < t; s++) {
+      console.log(arr[s*2+1].replace('/ /g',''));
+    }
+});
