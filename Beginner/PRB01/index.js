ffa3c2bf31d27b3bb2b114384ca6f0200506af49
happytejasrathod <happytejasrathod@gmail.com>
2020-07-03 08:27:17 +0530

    Beginner - Primality Test

---
+++ Beginner/PRB01/index.js
@@ -0,0 +1,38 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var arr = '';
+process.stdin.on('data', function (chunk) {
+    arr += chunk;
+});
+
+process.stdin.on('end', function () {
+    arr = arr.split('\n');
+    var t = arr.shift();
+    for (var i = 0; i < t; i++) {
+      var n = parseInt(arr[i]);
+      test_prime(n) ? console.log('yes') : console.log('no');
+    }
+});
+
+function test_prime(n)
+{
+  if (n===1)
+  {
+    return false;
+  }
+  else if(n === 2)
+  {
+    return true;
+  }else
+  {
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
