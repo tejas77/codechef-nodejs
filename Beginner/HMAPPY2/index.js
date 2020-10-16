8e94452b64bf1307fd6792cffed72ee98b8f8e42
happytejasrathod <happytejasrathod@gmail.com>
2020-10-17 00:33:45 +0530

    Beginner - Appy and Contest Update

--- Beginner/HMAPPY2/index.js
+++ Beginner/HMAPPY2/index.js
@@ -10,19 +10,19 @@ process.stdin.on('end', function () {
     arr = arr.split('\n');
     let t = Number(arr.shift());
     loop: for (let s = 0; s < t; s++) {
-      const [n,a,b,k] = arr[s].split(' ').map(Number);
-      let least = check(a,b), count;
-      if (least === a) {
-        count = parseInt((n/b) - (n/a));
-      } else if (least === b) {
-        count = parseInt((n/a) - (n/b));
-      } else {
-        count = parseInt((n/a) + (n/b) - (2*(n/least)));
-      }
-      console.log(count >= k ? 'Win' : 'Lose');
+      const [n,a,b,k] = arr[s].trim().split(' ').map(Number);
+      let x = parseInt(n / a);
+      let y = parseInt(n / b);
+      let z = parseInt(n / lcm(a, b));
+      console.log((x + y - (2 * z)) >= k ? 'Win' : 'Lose');
     }
 });
 
-var check = (x, y) =>  {
-  return !(y%x) ? y : !(x%y) ? x : (x*y);
+var gcd = function(a, b) {
+  if (!b) return a;
+  return gcd(b, a % b);
+};
+
+var lcm = function(a, b) {
+  return Math.floor(a * b / gcd(a, b));
 };
